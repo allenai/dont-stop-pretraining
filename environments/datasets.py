31449036f54aad834b3ce256024616a6932bfc9d
kernelmachine <sgrangan01@gmail.com>
2020-04-16 11:23:41 -0700

    added ner

--- environments/datasets.py
+++ environments/datasets.py
@@ -1,39 +1,69 @@
+NER_DATASETS = {
+    "ncbi": {
+        "data_dir": "/home/suching/scibert/data/ner/NCBI-disease/",
+    },
+    "sciie": {
+        "data_dir": "/home/suching/scibert/data/ner/sciie/"
+    },
+    "jnlpba": {
+        "data_dir": "/home/suching/scibert/data/ner/JNLPBA/"
+    },
+    "bc5cdr": {
+        "data_dir": "/home/suching/scibert/data/ner/bc5cdr/"
+    }
+}
 
-DATASETS = {
-    "chemprot": {
-        "data_dir": "s3://suching-dev/textcat/science/chemprot/",
-        "dataset_size": 4169
+
+CLASSIFICATION_DATASETS = {
+    "hatespeech": {
+        "data_dir": "s3://suching-dev/textcat/twitter/hatespeech/",
     },
-    "rct-20k": {
-        "data_dir": "s3://suching-dev/textcat/science/rct-20k/",
-        "dataset_size": 180040
+    "ag": {
+        "data_dir": "s3://suching-dev/textcat/news/ag/"
     },
-    "rct-sample": {
-        "data_dir": "s3://suching-dev/textcat/science/rct-sample/",
-        "dataset_size": 500
+    "scicite": {
+        "data_dir": "s3://suching-dev/textcat/science/sci-cite/",
     },
     "citation_intent": {
-        "data_dir": "s3://suching-dev/textcat/science/citation_intent/",
-        "dataset_size": 1688
+        "data_dir": "s3://suching-dev/textcat/science/citation_intent/"
+    },
+    "chemprot": {
+        "data_dir": "s3://suching-dev/textcat/science/chemprot/"
     },
     "sciie": {
         "data_dir": "s3://suching-dev/textcat/science/sciie/",
-        "dataset_size": 3219
-    },
-    "ag": {
-        "data_dir": "s3://suching-dev/textcat/news/ag/",
-        "dataset_size": 115000
     },
     "hyperpartisan_news": {
         "data_dir": "s3://suching-dev/textcat/news/hyperpartisan_by_article/",
-        "dataset_size": 500
+    },
+    "biased_news": {
+        "data_dir": "s3://suching-dev/textcat/news/biased_news/",
     },
     "imdb": {
         "data_dir": "s3://suching-dev/textcat/reviews/imdb/",
-        "dataset_size": 20000
     },
     "amazon": {
         "data_dir": "s3://suching-dev/textcat/reviews/amazon/",
-        "dataset_size": 115251
+    },
+    "yelp": {
+        "data_dir": "s3://suching-dev/textcat/reviews/yelp/",
+    },
+    "twitter_sentiment": {
+        "data_dir": "s3://suching-dev/textcat/twitter/semeval_2017_ task_4A/",
+    },
+    "twitter_irony_task_a": {
+        "data_dir": "s3://suching-dev/textcat/twitter/semeval_2018_task3_irony_detection/task_a/",
+    },
+    "twitter_irony_task_b": {
+        "data_dir": "s3://suching-dev/textcat/twitter/semeval_2018_task3_irony_detection/task_b/",
+    },
+    "rct-20k": {
+        "data_dir": "s3://suching-dev/textcat/science/rct-20k/",
+    },
+    "cs-abstruct": {
+        "data_dir": "s3://suching-dev/textcat/science/csabstruct-reformat/",
     }
 }
+
+
+DATASETS = {"NER": NER_DATASETS, "CLASSIFICATION": CLASSIFICATION_DATASETS}
