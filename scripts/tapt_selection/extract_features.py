f7b36cd8e28825eacd201b1ab4b3179ffd6c0327
kernelmachine <sgrangan01@gmail.com>
2020-07-17 15:30:19 -0700

    hf api works

--- scripts/tapt_selection/extract_features.py
+++ scripts/tapt_selection/extract_features.py
@@ -43,7 +43,8 @@ def predict_json(predictor, batch_data):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("--model", type=str, required=True)
+    parser.add_argument("--model", type=str, required=True, help="path to vampire model archive (e.g. /path/to/model.tar.gz)"
+                                                                 " or huggingface model name (e.g. roberta-base) ")
     parser.add_argument("--output_file", type=str, required=True, help='path to output')
     parser.add_argument("--input_file", type=str, required=True, help='path to output')
     parser.add_argument('--batch_size', type=int, required=False, default=64)
@@ -65,30 +66,31 @@ if __name__ == '__main__':
             file_length += 1
 
     if 'tar.gz' in args.model:
-        predictor = model
+        predictor = model.model
     else:
         predictor = None
-    file_iterator = lazy_groups_of(get_json_data(args.input_file, predictor=predictor.model), args.batch_size)
+    file_iterator = lazy_groups_of(get_json_data(args.input_file, predictor=predictor), args.batch_size)
     
     for batch_json in tqdm(file_iterator, total=file_length // args.batch_size):
         if 'tar.gz' in args.model:
-            batch_vectors = predictor.predict(batch_json, batch=True, scalar_mix=True)
+            batch_vectors = predictor.extract_features(batch_json, batch=True, scalar_mix=True)
             for vector in batch_vectors:
                 vectors.append(vector)
         else:
             lines = [x['text'] for x in batch_json]
             input_ids = tokenizer.batch_encode_plus(lines,
-                                                    add_special_tokens=tokenizer.add_special_tokens,
-                                                    truncation=True,
-                                                    max_length=tokenizer.max_model_input_sizes[args.model],
-                                                    return_tensors='pt',
-                                                    padding=True)
+                                                add_special_tokens=tokenizer.add_special_tokens,
+                                                truncation=True,
+                                                max_length=100,
+                                                pad_to_max_length=True,
+                                                return_tensors='pt',
+                                                padding=True)
             if args.device >= 0:
                 input_ids = input_ids.to(model.device)         
             with torch.no_grad():
                 out = model(**input_ids)
                 vectors_ = out[0][:, 0, :]  # Models outputs are now tuples
-            vectors.extend(vectors_)
+            vectors.append(vectors_)
         indices = torch.IntTensor([x['index'] for x in batch_json]).unsqueeze(-1)
         ids.append(indices)
     torch.save((torch.cat(ids, 0).cpu(), torch.cat(vectors, 0).cpu()), args.output_file)
