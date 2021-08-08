266269faca8645482eef2e710d916607ea2c71d2
Suchin <sgrangan01@gmail.com>
2021-08-08 13:32:54 -0700

    Update datasets.py

--- environments/datasets.py
+++ environments/datasets.py
@@ -1,39 +1,39 @@
 
 DATASETS = {
     "chemprot": {
-        "data_dir": "https://allennlp.s3-us-west-2.amazonaws.com/dont_stop_pretraining/data/chemprot/",
+        "data_dir": "https://s3-us-west-2.amazonaws.com/allennlp/dont_stop_pretraining/data/chemprot/",
         "dataset_size": 4169
     },
     "rct-20k": {
-        "data_dir": "https://allennlp.s3-us-west-2.amazonaws.com/dont_stop_pretraining/data/rct-20k/",
+        "data_dir": "https://s3-us-west-2.amazonaws.com/allennlp/dont_stop_pretraining/data/rct-20k/",
         "dataset_size": 180040
     },
     "rct-sample": {
-        "data_dir": "https://allennlp.s3-us-west-2.amazonaws.com/dont_stop_pretraining/data/rct-sample/",
+        "data_dir": "https://s3-us-west-2.amazonaws.com/allennlp/dont_stop_pretraining/data/rct-sample/",
         "dataset_size": 500
     },
     "citation_intent": {
-        "data_dir": "https://allennlp.s3-us-west-2.amazonaws.com/dont_stop_pretraining/data/citation_intent/",
+        "data_dir": "https://s3-us-west-2.amazonaws.com/allennlp/dont_stop_pretraining/data/citation_intent/",
         "dataset_size": 1688
     },
     "sciie": {
-        "data_dir": "https://allennlp.s3-us-west-2.amazonaws.com/dont_stop_pretraining/data/sciie/",
+        "data_dir": "https://s3-us-west-2.amazonaws.com/allennlp/dont_stop_pretraining/data/sciie/",
         "dataset_size": 3219
     },
     "ag": {
-        "data_dir": "https://allennlp.s3-us-west-2.amazonaws.com/dont_stop_pretraining/data/ag/",
+        "data_dir": "https://s3-us-west-2.amazonaws.com/allennlp/dont_stop_pretraining/data/ag/",
         "dataset_size": 115000
     },
     "hyperpartisan_news": {
-        "data_dir": "https://allennlp.s3-us-west-2.amazonaws.com/dont_stop_pretraining/data/hyperpartisan_news/",
+        "data_dir": "https://s3-us-west-2.amazonaws.com/allennlp/dont_stop_pretraining/data/hyperpartisan_news/",
         "dataset_size": 500
     },
     "imdb": {
-        "data_dir": "https://allennlp.s3-us-west-2.amazonaws.com/dont_stop_pretraining/data/imdb/",
+        "data_dir": "https://s3-us-west-2.amazonaws.com/allennlp/dont_stop_pretraining/data/imdb/",
         "dataset_size": 20000
     },
     "amazon": {
-        "data_dir": "https://allennlp.s3-us-west-2.amazonaws.com/dont_stop_pretraining/data/amazon/",
+        "data_dir": "https://s3-us-west-2.amazonaws.com/allennlp/dont_stop_pretraining/data/amazon/",
         "dataset_size": 115251
     }
 }
