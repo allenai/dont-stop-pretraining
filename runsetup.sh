70ae87d4040f327e36facf6b0da88a5ab42eaa67
ldery <derylucio@gmail.com>
2021-10-28 15:08:57 -0400

    updated run_setup.sh - code still reproduces good vbasic1 numbers

--- runsetup.sh
+++ runsetup.sh
@@ -6,34 +6,38 @@ taskdata='/home/ldery/internship/dsp/datasets/citation_intent/train.txt'
 domaindata='/home/ldery/internship/dsp/datasets/citation_intent/domain.1xTAPT.txt'
 
 gpuid=$1
-pergpubsz=43
-iterbsz=43
-gradAccumSteps=6
-lr=1e-4 #1e-3 # 1e-4 #
-classflr=1e-3 # 1e-5 #
+pergpubsz=32
+iterbsz=32
+gradAccumSteps=8
+lr=1e-4
+classflr=1e-3
 patience=10
 classfdp=0.3
-devWd=0.1
-devlr=$2 #1e-3
-spconfig='vbasic'
+devlr=1e-2
 iters=150
-soptlr=$3 #5e-2
 wfrac=0.06
-basewd=0.01
 devftiters=10
 devbsz=32
-start=$4
-end=$5
-mstep=2
-ext=$6
+start=$2
+end=$3
+ext=$4
 cfwd=0.1
+temp=0.5
+soptlr=1e-1
+auxlr=1e-1
+basewd=0.01
+devWd=0.1
+mstep=3
+spconfig='vbasic1'
+nconfigsamples=6
+
 
 for k in $(seq $start $end)
 do
 	echo $k
-	base='soptlr='$soptlr'.lr='$lr'.classflr='$classflr'.seed='$k'.classfdp='$classfdp'.spconfig='$spconfig'.warmupfrac='$wfrac'.devlr='$devlr'.metastepevery'$mstep'.cfwd='$cfwd'.devftiters='$devftiters'.'$ext
-# 	'.'$ext
+	base='HeadNorm='$maxHeadNorm'.nconfigsamples='$nconfigsamples'.devwd='$devWd'.basewd='$basewd'.prim-aux-lr='$soptlr'.auxiliaries-lr='$auxlr'.lr='$lr'.classflr='$classflr'.seed='$k'.classfdp='$classfdp'.spconfig='$spconfig'.warmupfrac='$wfrac'.devlr='$devlr'.metastepevery'$mstep'.cfwd='$cfwd'.devftiters='$devftiters'.temp'$temp'.'$ext
 	echo $base
-	CUDA_VISIBLE_DEVICES=$gpuid python -u -m scripts.autoaux --prim-task-id $primtaskid --train_data_file $trainfile --dev_data_file $devfile --test_data_file $testfile --output_dir autoaux_outputs/$primtaskid/$base --model_type roberta-base --model_name_or_path roberta-base  --tokenizer_name roberta-base --per_gpu_train_batch_size $pergpubsz  --gradient_accumulation_steps $gradAccumSteps --do_train --learning_rate $lr --block_size 512 --logging_steps 5000 --classf_lr $classflr --classf_patience $patience --num_train_epochs $iters  --classifier_dropout $classfdp --overwrite_output_dir --classf_iter_batchsz  $iterbsz --classf_ft_lr 1e-6 --classf_max_seq_len 512 --seed $k  --classf_dev_wd $devWd --classf_dev_lr $devlr -searchspace-config $spconfig -task-data $taskdata -in-domain-data $domaindata -num-config-samples 6 --dev_batch_sz $devbsz --eval_every 30 -searchopt-lr $soptlr --classf_warmup_frac $wfrac --classf_wd $cfwd --base_wd $basewd -use-factored-model --dev_fit_iters $devftiters -step-meta-every $mstep --share-output-heads &> logfldrs/$base'.txt'
-	#-use-EG
+	CUDA_VISIBLE_DEVICES=$gpuid python -u -m scripts.autoaux --prim-task-id $primtaskid --train_data_file $trainfile --dev_data_file $devfile --test_data_file $testfile --output_dir autoaux_outputs/$primtaskid/$base --model_type roberta-base --model_name_or_path roberta-base  --tokenizer_name roberta-base --per_gpu_train_batch_size $pergpubsz  --gradient_accumulation_steps $gradAccumSteps --do_train --learning_rate $lr --block_size 512 --logging_steps 5000 --classf_lr $classflr --classf_patience $patience --num_train_epochs $iters  --classifier_dropout $classfdp --overwrite_output_dir --classf_iter_batchsz  $iterbsz --classf_ft_lr 1e-6 --classf_max_seq_len 512 --seed $k  --classf_dev_wd $devWd --classf_dev_lr $devlr -searchspace-config $spconfig -task-data $taskdata -in-domain-data $domaindata -num-config-samples $nconfigsamples --dev_batch_sz $devbsz --eval_every 30 -prim-aux-lr $soptlr -auxiliaries-lr $auxlr --classf_warmup_frac $wfrac --classf_wd $cfwd --base_wd $basewd --dev_fit_iters $devftiters -step-meta-every $mstep -use-factored-model --share-output-heads -token_temp $temp &> logfldrs/$base'.txt'
 done
+
+
